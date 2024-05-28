755efdfb7b08f00139e76087bcccfb3a004fc239
Jinjie <olivernova1998@gmail.com>
2024-05-29 02:25:37 +0800

    update leaderboard

--- static/utils/xlsx2htmltab.py
+++ static/utils/xlsx2htmltab.py
@@ -46,6 +46,28 @@ meta_dicts = {
     'Qwen1.5-4B-Chat': {'url': 'https://huggingface.co/Qwen/Qwen1.5-4B-Chat'},
     'JetMoE-8B-Chat': {'url': 'https://huggingface.co/jetmoe/jetmoe-8b-chat'},
     'MPT-7B-Chat': {'url': 'https://huggingface.co/mosaicml/mpt-7b-chat'},
+    
+    'LLaMA-3-70B': {'url': 'https://huggingface.co/meta-llama/Meta-Llama-3-70B'},
+    'Qwen1.5-72B': {'url': 'https://huggingface.co/Qwen/Qwen1.5-72B'},
+    'Yi-34B': {'url': 'https://huggingface.co/01-ai/Yi-1.5-34B'},
+    'Qwen1.5-32B': {'url': 'https://huggingface.co/Qwen/Qwen1.5-32B'},
+    'Mixtral-8x7B': {'url': 'https://huggingface.co/mistralai/Mixtral-8x7B-v0.1'},
+    'LLaMA-2-70B': {'url': 'https://huggingface.co/meta-llama/Llama-2-70b'},
+    'Qwen1.5-MoE-A2.7B': {'url': 'https://huggingface.co/Qwen/Qwen1.5-MoE-A2.7B'},
+    'Qwen1.5-7B': {'url': 'https://huggingface.co/Qwen/Qwen1.5-7B'},
+    'LLaMA-3-8B': {'url': 'https://huggingface.co/meta-llama/Meta-Llama-3-8B'},
+    'Mistral-7B': {'url': 'https://huggingface.co/mistralai/Mistral-7B-v0.1'},
+    'Gemma-7B': {'url': 'https://huggingface.co/google/gemma-7b'},
+    'Yi-6B': {'url': 'https://huggingface.co/01-ai/Yi-6B'},
+    'Qwen1.5-4B': {'url': 'https://huggingface.co/Qwen/Qwen1.5-4B'},
+    'JetMoE-8B': {'url': 'https://huggingface.co/jetmoe/jetmoe-8b'},
+    'DeepSeek-7B': {'url': 'https://huggingface.co/deepseek-ai/deepseek-llm-7b-base'},
+    'Phi-2': {'url': 'https://huggingface.co/microsoft/phi-2'},
+    'DeepSeekMoE-16B': {'url': 'https://huggingface.co/deepseek-ai/deepseek-moe-16b-base'},
+    'LLaMA-2-7B': {'url': 'https://huggingface.co/meta-llama/Llama-2-7b'},
+    'Gemma-2B': {'url': 'https://huggingface.co/google/gemma-2b'},
+    'OLMo-7B': {'url': 'https://huggingface.co/allenai/OLMo-7B'},
+    'MPT-7B': {'url': 'https://huggingface.co/mosaicml/mpt-7b'},
 }
 
 proprietary_models = [
@@ -67,30 +89,61 @@ proprietary_models = [
     'Reka Edge'
 ]
 
-def generate_html_table(input_file, output_file):
+
+def generate_html_table(input_file, output_file, table_id='table1'):
     # Read the Excel file
     df = pd.read_excel(input_file, header=None)
+    # print(df)
+    for col_idx in df.columns[1:]:
+    # Scores are in the rows below the first one
+        scores = pd.to_numeric(df.iloc[1:, col_idx], errors='coerce')
+        # Check if there are enough scores to proceed
+        if scores.dropna().empty:
+            continue
+        # print(scores)
+        # Find the index of the highest score
+        first_max_idx = scores.idxmax()  # Adjust index for header row
+        # Apply bold formatting
+        df.iloc[first_max_idx, col_idx] = f"<b>{df.iloc[first_max_idx, col_idx]}</b>"
+        
+        # Nullify the highest score to find the second highest
+        scores[first_max_idx] = pd.NA  # Adjust index back for zero-based indexing
+        # Check again for remaining valid scores
+        if scores.dropna().empty:
+            continue
+        
+        second_max_idx = scores.idxmax()  # Adjust index for header row
+        # Apply underline formatting
+        if pd.notna(second_max_idx):
+            df.iloc[second_max_idx, col_idx] = f"<u>{df.iloc[second_max_idx, col_idx]}</u>"
+
+        
     
     # for t in df.iloc[1:, 0]:
     #     print(f"'{t}': {{'url': ''}},")
     
     # Update the first column to include the hyperlink and formatting
-    df.iloc[1:, 0] = df.iloc[1:, 0].apply(lambda x: f'''<td style="text-align: left;width: 200px;"><a href="{meta_dicts[x]['url']}"><b>{x}</b></a></td>''')
+    df.iloc[1:, 0] = df.iloc[1:, 0].apply(lambda x: f'''<td style="text-align: left;width: 200px;"><a href="{meta_dicts[x]['url']}" target="_blank"><b>{x}</b></a></td>''')
     
-    df.iloc[0] = df.iloc[0].apply(lambda x: f'<td class="js-sort-number"><strong>{x}</strong></td>')
+    df.iloc[0] = df.iloc[0].apply(lambda x: f'<td class="js-sort-number" style="background-color:#333333b2;"><strong><a  style="color:#ffffffff;">{x}</a></strong></td>')
     
     # Convert the DataFrame to an HTML string, without headers and index
     html_table = df.to_html(index=False, escape=False, header=False)
     # print(type(html_table))
     # Add the table with specific class and ID
-    html_table = html_table.replace('<table border="1" class="dataframe">', '<table class="js-sort-table" id="table1">').replace('<td><td ', '<td ').replace('</td></td>', '</td>')
+    if table_id=='table1':
+        html_table = html_table.replace('<table border="1" class="dataframe">', '<table class="js-sort-table" id="table1">').replace('<td><td ', '<td ').replace('</td></td>', '</td>')
+    elif table_id=='table2':
+        html_table = html_table.replace('<table border="1" class="dataframe">', '<table class="js-sort-table hidden" id="table2">').replace('<td><td ', '<td ').replace('</td></td>', '</td>')
+    else:
+        raise ValueError(f'Invalid table_id: {table_id}.')
     
     # Remove thead and tbody tags
     html_table = html_table.replace('<thead>', '').replace('</thead>', '').replace('nan', '').replace(' (Mixed)', '<br>(Mixed)')
     html_table = html_table.replace('<tbody>', '').replace('</tbody>', '')
 
     # Replace the default <tr>, <th>, and <td> to include style
-    pattern = r'(<tr>\n\s+<td style="text-align: left;width: 200px;"><a href="([^"]+)"><b>(.*?)</b></a></td>)'
+    pattern = r'(<tr>\n\s+<td style="text-align: left;width: 200px;"><a href="([^"]+)" target="_blank"><b>(.*?)</b></a></td>)'
     matches = re.findall(pattern, html_table)
     # print(matches)
     for match in matches:
@@ -100,9 +153,9 @@ def generate_html_table(input_file, output_file):
                 is_pm = True
                 break
         if is_pm:
-            html_table = html_table.replace(match[0], match[0].replace('<tr>\n', '<tr style="background-color: rgba(117, 209, 215, 0.1);">\n'))
+            html_table = html_table.replace(match[0], match[0].replace('<tr>\n', '<tr style="background-color: #dae7d8;">\n'))
         else:
-            html_table = html_table.replace(match[0], match[0].replace('<tr>\n', '<tr style="background-color: rgba(255, 208, 80, 0.15);">\n'))
+            html_table = html_table.replace(match[0], match[0].replace('<tr>\n', '<tr style="background-color: #efefefff;">\n'))
         
     # html_table = html_table.replace('<tr>', '<tr style="background-color: rgba(255, 208, 80, 0.15);">')
     # html_table = html_table.replace('<td>', '<td style="padding: 8px; border: 1px solid #ddd; text-align: center;">')
@@ -112,4 +165,5 @@ def generate_html_table(input_file, output_file):
     with open(output_file, "w") as file:
         file.write(html_table)
 
-generate_html_table('static/utils/scores.xlsx', 'static/utils/output_html_tab.html')
+generate_html_table('static/utils/scores_chat.xlsx', 'static/utils/output_html_tab_chat.html')
+generate_html_table('static/utils/scores_base.xlsx', 'static/utils/output_html_tab_base.html', table_id='table2')
